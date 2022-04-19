fff1e0fbbd64d0c677494f21e88a594de1eb33a3
@owise1 <owise1@gmail.com>
2022-04-19 15:08:25 -0400

    cid file, symlinks

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
+const { CID } = require('multiformats/cid')
+const raw = require('multiformats/codecs/raw')
+const { sha256 } = require('multiformats/hashes/sha2')
+const { TextEncoder } = require('util')
+const fs = require('fs/promises')
 const express = require('express')
+const serveStatic = require('serve-static')
 const fileUpload = require('express-fileupload')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const { mapObjIndexed, pipe, flatten, values } = require('ramda')
+
+const FILE_DIR = 'ic'
+
 
 const app = express()
-
 app.use(fileUpload({
   createParentPath: true,
   useTempFiles: true,
   tempFileDir: 'tmp/'
 }))
-
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(serveStatic(FILE_DIR))
 
 app.post('/ic', async (req, res) => {
   try {
-    if (!req.files) {
-      res.send({
-        status: false,
-        message: 'No file uploaded'
-      })
-    } else {
-      console.log(req.files)
-
+    if (req.files) {
+      const files = await Promise.all(values(mapObjIndexed(async (file, key) => {
+        const ret = []
+        const str = await fs.readFile(file.tempFilePath, 'utf8')
+        const bytes = new TextEncoder('utf8').encode(str) 
+        const hash = await sha256.digest(bytes)
+        const cid = CID.create(1, raw.code, hash)
+        const filePath = `${key}/${cid.toString()}.ic`
+        ret.push(filePath)
+        await file.mv(FILE_DIR + '/' + filePath)
+        // they also want a symlink
+        if (req.body[file.name]) {
+          const symlink = `${key}/${req.body[file.name]}` 
+          try{
+            await fs.unlink(`${FILE_DIR}/${symlink}`)
+          } catch (e) {
+            // do nothing
+          }
+          await fs.symlink(__dirname + '/' + FILE_DIR + '/' + filePath, `${FILE_DIR}/${symlink}`)
+          ret.push(symlink)
+        }
+        return ret 
+      }, req.files)))
       //send response
       res.send({
-        status: true,
-        message: 'File is uploaded',
-        data: {
-          // name: avatar.name,
-          // mimetype: avatar.mimetype,
-          // size: avatar.size
-        }
+        ok: true,
+        files: flatten(files)
       })
     }
   } catch (err) {
